05a8ec6bb727b3e3ca499ff6cf4226e239e9347f
M4tisPto <matis.pto.104250@gmail.com>
2026-04-06 08:32:19 -0400

    re-do shiet

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,33 +1,85 @@
 extends CharacterBody2D
 
+
+
+# Velocidades iniciales
 @export var walk_speed = 650.0
 @export var run_speed = 1000.0
-@export var acceleration = 0.1
-@export var deceleration = 0.1
+@export_range(0, 1) var acceleration = 0.1
+@export_range(0, 1) var deceleration = 0.1
+
+# Modelo 3D
+@onready var player: Node3D = $SubViewport/Node3D/low_poly_prot
+# Camera_shake
+@onready var camera_2d: Camera2D = $Camera2D
+# Salto
 @export var jump_force = -400.0
 
+#Ground slam
 const GROUND_SLAM_SPEED = 1200.0
-
+var is_slamming := false
+# Gravedad
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Doble salto
 var jumps_left: int = 0
 const TOTAL_JUMPS: int = 2
-var is_slamming: bool = false
+var was_in_air: bool = false
 
-var papas = 10
 
-@onready var player_model: Node3D = $SubViewport/Node3D/low_poly_prot
-@onready var camera_2d: Camera2D = $Camera2D
-@onready var state_machine: StateMachine = $StateMachine
-
-func _ready():
-	for state in state_machine.get_children():
-		if state is State:
-			state.state_machine = state_machine
-			state.player = self
-	state_machine.change_state($StateMachine/Idle)
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
+	
+	if was_in_air and is_on_floor():
+		was_in_air = false
+	if Input.is_action_just_pressed("ground_slam") and not is_on_floor():
+		is_slamming = true
+		velocity.y = GROUND_SLAM_SPEED
+		velocity.x = 0
+	
+	if is_on_floor() and is_slamming:
+		is_slamming = false
+		camera_2d.screen_shake(10.0, 0.2)
+	
+	if is_on_floor():
+		jumps_left = TOTAL_JUMPS
+		is_slamming = false
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_force
+			jumps_left = TOTAL_JUMPS - 1
+		else:
+			if jumps_left > 0:
+				velocity.y = jump_force
+				jumps_left -= 1
+				
+				if jumps_left == 0:
+					velocity.y = jump_force
+	
+	
+	
+	var speed
+	if Input.is_action_pressed("sprint"):
+		speed = run_speed
+	else:
+		speed = walk_speed
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction :float = Input.get_axis("move_left", "move_right")
+	
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
+		if direction > 0:
+			player.rotation.y = PI/3
+		else:
+			player.rotation.y = -PI/3
+	else:
+		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
+
+
 
 	move_and_slide()
